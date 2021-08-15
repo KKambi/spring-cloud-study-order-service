46369e95ffdf79fe68d4cf6c6150d114e5087b3b
KKambi <bok03220@gmail.com>
2021-08-15 15:40:07 +0900

    feat: 로드 밸런싱으 확인하기 위한 포트 로깅

--- src/main/java/com/example/orderservice/WelcomeController.java
+++ src/main/java/com/example/orderservice/WelcomeController.java
@@ -1,13 +1,23 @@
 package com.example.orderservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/order-service")
 public class WelcomeController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(@RequestHeader("order-request") String header) {
@@ -15,7 +25,9 @@ public class WelcomeController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is message from Order service";
+    public String check(HttpServletRequest request) {
+        log.info("port: {}", request.getServerPort());
+        return String.format("Hi, there. This is message from Order service, port: %s",
+                env.getProperty("local.server.port"));
     }
 }
